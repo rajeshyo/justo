130ad916a18fdd8c69ae093754727d81eec49397
rajeshyo <rajesh95mailbox@gmail.com>
2021-09-08 18:50:34 +0530

    order listing and cancel order

--- src/app/order/order.page.ts
+++ src/app/order/order.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, MenuController, ToastController, AlertController, LoadingController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpHeaders, HttpErrorResponse} from '@angular/common/http';
+import { ToastService } from '../services/toast.service';
 
 @Component({
   selector: 'app-order',
@@ -6,10 +11,80 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order.page.scss'],
 })
 export class OrderPage implements OnInit {
+  data: any;
+  topdata1=[]
+  orderlistdata=[]
+  orderid:any
+  constructor(public navCtrl: NavController,
+    public menuCtrl: MenuController,
+    public toastCtrl: ToastController,
+    private toastService: ToastService,
+    public alertCtrl: AlertController,
+    public loadingCtrl: LoadingController,
+    public activatedRouter:ActivatedRoute,
+    private http: HttpClient,
 
-  constructor() { }
+  ) { }
 
   ngOnInit() {
+    this.orderlist()
+  }
+  orderlist(){
+    let url = environment.baseurl
+    const session = localStorage.getItem('session');
+    const userid = localStorage.getItem('userid');
+    const orderdetails = localStorage.getItem('orderdetails');
+  
+    var formdata = new FormData();
+    formdata.append('_operation','getOrderListing');
+    formdata.append('_session',session);
+    
+    formdata.append('userId',userid);
+    // formdata.append('values',"this.cart");
+   
+    this.http.post( url,formdata,{})
+    .toPromise()
+    .then(response => {
+      this.data = response;
+      this.orderlistdata =this.data.result.orders
+      this.orderid =this.data.result.orders
+      console.log("orderlist",this.data.result.orders);
+     
+      // this.addtocart()
+      return this.orderlistdata;
+  
+    })
+    .catch(console.log);
   }
 
+  
+  cancelorder(id){
+    let url = environment.baseurl
+    const session = localStorage.getItem('session');
+    const userid = localStorage.getItem('userid');
+    const orderdetails = localStorage.getItem('orderdetails');
+  
+    var formdata = new FormData();
+    formdata.append('_operation','cancelOrder');
+    formdata.append('_session',session);
+    
+    formdata.append('userId',userid);
+    formdata.append('sostatus',"Cancelled");
+    formdata.append('module', 'SalesOrder');
+    formdata.append('action', 'SaveAjax');
+    formdata.append('orderId', id );
+   
+    this.http.post( url,formdata,{})
+    .toPromise()
+    .then(response => {
+      this.data = response;
+      this.topdata1 =this.data.result.orders;
+      this.orderlist();
+      this.toastService.presentToast('Cancel Sucessfull');
+      console.log("orderlist",this.data.result.orders);
+      // return this.topdata1;
+  
+    })
+    .catch(console.log);
+  }
 }
